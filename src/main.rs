3ac329b8b0717af80880c500a6859ce7cb334ce5
Murray Todd Williams <murray.williams@accenture.com>
2024-06-10 15:00:12 -0500

    A little clean-up and docs

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,42 @@ use bsp::hal::{
     watchdog::Watchdog,
 };
 
-fn tmp36_f(adc_reading: u16) -> f32 {
-    let voltage: f32 = adc_reading as f32 * 3.3 / 4096.0;
-    let c = (100.0 * voltage) - 50.0;
-    return (c * 9.0 / 5.0) + 32.0;
+const REFERENCE_VOLTAGE: f32 = 3.3;
+const STEPS_12BIT: f32 = 4096 as f32;
+
+/// Basic Celsius-to-Fahrenheit conversion
+fn c_to_f(c: f32) -> f32 {
+    (c * 9.0 / 5.0) + 32.0
 }
 
+/// Convert ADC binary value to a float voltage value.
+///
+/// The ADC has a 12-bit resolution of voltage, meaning that there
+/// are 2^12 or 4096 unique levels from OFF (0V) to FULL (3V). This
+/// function converts the ADC reading into a float measurement in volts.
+fn adc_reading_to_voltage(reading_12bit: u16) -> f32 {
+    (reading_12bit as f32 / STEPS_12BIT) * REFERENCE_VOLTAGE
+}
+
+/// Convert the voltage from a TMP36 sensor into a temperature reading.
+///
+/// The sensor returns 0.5V at 0°C and voltage changes ±0.01V for every
+/// degree Celcius with higher temps resolting in higher voltages within
+/// the range of -40°C to 125°C.
+fn tmp36_f(adc_reading: u16) -> f32 {
+    let voltage: f32 = adc_reading_to_voltage(adc_reading);
+    let c = (100.0 * voltage) - 50.0;
+    c_to_f(c)
+}
+
+/// Convert the voltage from the onboard temp sensor into a temp reading.
+///
+/// From §4.9.5 from the rp2040-datasheet.pdf, the temperature can be
+/// approximated as T = 27 - (ADC_voltage - 0.706) / 0.001721.
 fn chip_f(adc_reading: u16) -> f32 {
-    let voltage: f32 = adc_reading as f32 * 3.3 / 4096.0;
+    let voltage: f32 = adc_reading_to_voltage(adc_reading);
     let c: f32 = 27.0 - ((voltage - 0.706) / 0.001721);
-    return (c * 9.0 / 5.0) + 32.0;
+    c_to_f(c)
 }
 
 #[entry]
@@ -88,7 +114,7 @@ fn main() -> ! {
 
     let mut mcp9808 = MCP9808::new(i2c);
     let mut mcp9808_conf = mcp9808.read_configuration().unwrap();
-    mcp9808_conf.set_shutdown_mode(mcp9808::reg_conf::ShutdownMode::Shutdown);
+    mcp9808_conf.set_shutdown_mode(mcp9808::reg_conf::ShutdownMode::Continuous);
     mcp9808.write_register(mcp9808_conf).unwrap();
 
     let mut adc = bsp::hal::Adc::new(pac.ADC, &mut pac.RESETS);
@@ -105,16 +131,18 @@ fn main() -> ! {
         led_pin.set_low().unwrap();
         delay.delay_ms(500);
 
-        let temp = mcp9808.read_temperature().unwrap();
-        let reading: f32 = temp.get_celsius(ResolutionVal::Deg_0_0625C);
-        let ftemp: f32 = (reading * (9.0 / 5.0)) + 32.0;
-        info!("Temperature reading: {}℉", ftemp);
-
-        // note: top voltage = 4096 because readings are 12-bit
-        let chip_temp: u16 = adc.read(&mut rp2040_temp_sensor).unwrap();
-        let tmp36_temp: u16 = adc.read(&mut adc_pin_0).unwrap();
-        info!("TMP36 readering: {}℉", tmp36_f(tmp36_temp));
-        info!("Chip Temp reading: {}℉", chip_f(chip_temp));
+        let mcp9808_reading_c: f32 = mcp9808
+            .read_temperature()
+            .unwrap()
+            .get_celsius(ResolutionVal::Deg_0_0625C);
+        let chip_voltage_24bit: u16 = adc.read(&mut rp2040_temp_sensor).unwrap();
+        let tmp36_voltage_24bit: u16 = adc.read(&mut adc_pin_0).unwrap();
+        info!(
+            "Temp readings:  MCP9808: {}°F, TMP36: {}°F, OnChip: {}°F",
+            c_to_f(mcp9808_reading_c),
+            chip_f(chip_voltage_24bit),
+            tmp36_f(tmp36_voltage_24bit)
+        );
     }
 }
 
