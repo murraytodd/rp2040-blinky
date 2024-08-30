def0ea7ebc30b662bd89053c812a06d4e5fa63d4
Murray Todd Williams <murray.williams@accenture.com>
2024-08-30 09:33:39 -0500

    Trying to get embedded-test working

--- build.rs
+++ build.rs
@@ -23,6 +23,8 @@ fn main() {
         .unwrap();
     println!("cargo:rustc-link-search={}", out.display());
 
+    println!("cargo::rustc-link-arg-tests=-Tembedded-test.x");
+
     // By default, Cargo will re-run a build script whenever
     // any file in the project changes. By specifying `memory.x`
     // here, we ensure the build script is only re-run when
---
+++ tests/blinky_test.rs
@@ -0,0 +1,11 @@
+#![no_std]
+#![no_main]
+
+#[cfg(test)]
+#[embedded_test::tests]
+mod tests {
+    #[test]
+    fn does_nothing() {
+        assert!(true)
+    }
+}
